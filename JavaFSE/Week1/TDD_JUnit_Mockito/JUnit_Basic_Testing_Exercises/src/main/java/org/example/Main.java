1031b50bde63ee43acd570bb65695b5eddc3fca3
shenivinclement <shenivinclement1023.sse@saveetha.com>
2026-07-14 19:20:28 +0530

    Mockito Exercises 1 & 2 (Mocking/Stubbing + Verifying Interactions)

--- JavaFSE/Week1/TDD_JUnit_Mockito/JUnit_Basic_Testing_Exercises/src/main/java/org/example/Main.java
+++ JavaFSE/Week1/TDD_JUnit_Mockito/JUnit_Basic_Testing_Exercises/src/main/java/org/example/Main.java
@@ -1,19 +1,21 @@
 package org.example;
 
 public class Main {
-    public int add(int a, int b) {
-        return a + b;
+    private UserRepository userRepository;
+
+    public Main() {}
+
+    public Main(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
-    public int subtract(int a, int b) {
-        return a - b;
-    }
+    public int add(int a, int b) { return a + b; }
+    public int subtract(int a, int b) { return a - b; }
+    public boolean isEven(int number) { return number % 2 == 0; }
+    public String greet(String name) { return name == null ? null : "Hello, " + name; }
 
-    public boolean isEven(int number) {
-        return number % 2 == 0;
-    }
-
-    public String greet(String name) {
-        return name == null ? null : "Hello, " + name;
+    public String getUserGreeting(int userId) {
+        String user = userRepository.findUserById(userId);
+        return user == null ? "User not found" : "Hello, " + user;
     }
 }
---
+++ JavaFSE/Week1/TDD_JUnit_Mockito/JUnit_Basic_Testing_Exercises/src/main/java/org/example/UserRepository.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public interface UserRepository {
+    String findUserById(int id);
+}
