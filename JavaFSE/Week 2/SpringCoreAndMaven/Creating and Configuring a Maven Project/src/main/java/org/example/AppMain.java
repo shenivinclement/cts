cd07cd993a6375d145e54390296697dff96570b4
shenivinclement <shenivinclement1023.sse@saveetha.com>
2026-07-16 00:45:20 +0530

    Spring Core and Maven: Exercises 1, 2, and 4 complete

---
+++ JavaFSE/Week 2/SpringCoreAndMaven/Creating and Configuring a Maven Project/src/main/java/org/example/AppMain.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AppMain {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Greeting greeting = (Greeting) context.getBean("greetingBean");
+        greeting.showMessage();
+    }
+}
