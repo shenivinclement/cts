63be7be77e711a6dc3c34d13a403f1af4899988a
shenivinclement <shenivinclement1023.sse@saveetha.com>
2026-07-16 00:16:17 +0530

    Spring Core Exercise 1: Configuring a basic Spring application with XML bean config

---
+++ JavaFSE/Week 2/SpringCoreAndMaven/Creating-Configuring-Maven-Project/src/main/java/org/example/Greeting.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Greeting {
+
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void showMessage() {
+        System.out.println(message);
+    }
+}
