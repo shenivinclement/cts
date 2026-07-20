ec7f7f9535b82488452d80c567b5ebbf9da140c6
shenivinclement <shenivinclement1023.sse@saveetha.com>
2026-07-20 21:14:33 +0530

    React exercise: Forms validation - Name/Email/Password rules via handleChange and handleSubmit

---
+++ JavaFSE/Week 3/ReactProjects/mailregisterapp/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Register from './register';
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+          <Register />
+        </div>
+    );
+  }
+}
+
+export default App;
